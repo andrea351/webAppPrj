6d9705fcb0bfb657c82fce01431bd150bbfb1313
andrea351 <giuliotti.2107814@studenti.uniroma1.it>
2026-04-23 12:15:07 +0200

    correzzione qm in env

--- api/connessione.php
+++ api/connessione.php
@@ -1,17 +1,21 @@
 <?php
+
+// ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+
 // ===================== CONNESSIONE AL DATABASE =====================
 // Questo file viene incluso dagli altri script PHP per connettersi a MySQL.
-// Modifica le variabili in .env se cambi configurazione.
+// Modifica le variabili in ' .env ' se cambi configurazione.
 
-require __DIR__ . '/../vendor/autoload.php'; // Carica Composer autoload
+require __DIR__ . '/../vendor/autoload.php'; // Carica Composer AutoLoad
 use Dotenv\Dotenv;
 
-// Carica il file .env dalla root del progetto
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->safeLoad(); // safeLoad evita errori se il file .env non esiste
+// Carica il file ' .env ' dalla ROOT del PROGETTO
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../'); // $dotenv = Dotenv::createUnsafeImmutable(__DIR__ . '/../');
+$dotenv->safeLoad(); // safeLoad evita errori se il file ' .env ' non esiste
 
 // ===================== CONNESSIONE AL DATABASE =====================
-// Leggi le variabili dal .env
+// Leggi le variabili dal ' .env '
 $host   = $_ENV['DB_HOST'] ?? '127.0.0.1';
 $dbname = $_ENV['DB_NAME'] ?? 'cinemood';
 $user   = $_ENV['DB_USER'] ?? 'root';
